e2fda263bc29c794216e20318ddfb31e3813acc9
Devyani <Devyani@Dev>
2024-03-24 16:50:02 +0530

    Adding Test Project

---
+++ PankajArtifactID/src/main/java/PankajPackage/Test1.java
@@ -0,0 +1,18 @@
+package PankajPackage;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void b() {
+		System.out.println("Hello TestNG");
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Main Method");
+
+	}
+
+}
